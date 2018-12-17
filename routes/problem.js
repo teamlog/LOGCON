a532e6b14cc203d8167da7c5366a0504b4c087a3
naruteride <tadanide@gmail.com>
2018-12-17 15:30:19 +0900

    방성훈 시작

--- routes/problem.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/problem/:num',(req,res) => {
-    const id = req.session.id;
-    
-})
--- routes/register.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('register', (req,res) => {
-    res.render('register.html');
-})
-.post('register', (req,res) => {
-    
-})
