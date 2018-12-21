3c70d5f442ad738e651393b20dcd3fdcc6c61af2
giljong <minjong5054@daum.net>
2018-12-21 14:52:01 +0900

    admin_id admin -> my id

--- routes/admin.js
+++ routes/admin.js
@@ -5,21 +5,21 @@ const router  = express.Router();
 const moment = require('moment');
 
 router.get('/',(req,res) => {
-    if(!(req.session.user == 'admin'))
+    if(!(req.session.user == 'minjong5054'))
         res.send('<script type="text/javascript">alert("관리자가 아니시군요?٩(๑`ȏ´๑)۶");window.location.href="/";</script>');
     else
         res.sendFile(path.join(__dirname,'../views', 'admin.html'));
 })
 
 router.get('/insertP',(req,res) => {
-    if(!(req.session.user === 'admin'))
+    if(!(req.session.user === 'minjong5054'))
         res.send('<script type="text/javascript">alert("관리자가 아니시군요?٩(๑`ȏ´๑)۶");window.location.href="/";</script>');
     else
         res.sendFile(path.join(__dirname,'../views', 'insertP.html'));
 })
 
 router.get('/upNotice',(req,res) => {
-    if(!(req.session.user === 'admin'))
+    if(!(req.session.user === 'minjong5054'))
         res.send('<script type="text/javascript">alert("관리자가 아니시군요?٩(๑`ȏ´๑)۶");window.location.href="/";</script>');
     else
     res.sendFile(path.join(__dirname,'../views', 'upNotice.html'));
