5063c82e9e61d96595828c00841042c2eb4f802c
naruteride <tadanide@gmail.com>
2018-12-22 00:20:52 +0900

    로그인 성공시 이메일 인증하라는 문구 수정

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -139,6 +139,7 @@ function commandInspection(text) {
             .then(function (myJSON) {
                 if (myJSON.success) {
                     history.innerHTML += ("<br>" + "Register success! Go to login page and log in.");
+                    history.innerHTML += ("<br>" + "If you go to any page, you will be taken to the email verification page. The verification code will be emailed in about 5 seconds.");
                 } else {
                     history.innerHTML += ("<br>" + "Register fail!");
                 }
