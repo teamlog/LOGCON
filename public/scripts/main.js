f2e9ff7925f6a0a6c9bf27826421f6cf89899a32
naruteride <tadanide@gmail.com>
2018-12-22 00:19:40 +0900

    회원가입하면 이메일 인증하라는 문구 뜨도록 수정

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -149,7 +149,8 @@ function commandInspection(text) {
             })
             .then(function (myJSON) {
                 if (myJSON.success) {
-                    history.innerHTML += ("<br>" + "Register success! Go to login page and log in.");
+                    history.innerHTML += ("<br>" + "Register success!");
+                    history.innerHTML += ("<br>" + "If you go to any page, you will be taken to the email verification page. The verification code will be emailed in about 5 seconds.");
                 } else {
                     history.innerHTML += ("<br>" + "Register fail!");
                 }
