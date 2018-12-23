91d3c43eb426056835212658b485553859baf09f
giljong <minjong5054@daum.net>
2018-12-23 16:25:12 +0900

    Update challenge.js

--- routes/challenge.js
+++ routes/challenge.js
@@ -23,34 +23,26 @@ router.get('/:num',(req,res) => {
         res.redirect('/');
 })
 router.post('/:num',(req,res) => {
-    console.log(req);
-    const pid = req.params.num;
     const user = req.session.user;
     const ans = req.body.answer;
-    console.log(ans);
-    function solveCheck(problemid, userid){
-        db.query('select * from Solved where PID = ? and USER = ?',[problemid,userid],(err,result) => {
-            if(err) throw err;
-            if(!(result.length === 0))
-                return 0;
-            else
-                return 1;
-        })
-    }
-    
-    db.query('select ANSWER,SCORE from Problems where ID = ?',pid,(err,result) => {
+    db.query('select ANSWER,SCORE from Problems where ID = ?',pnum,(err,result) => {
         if(err) throw err;
         if(result[0].ANSWER === ans){
-            if((solveCheck(pid,user) === 1)){
-                db.query('update Users set SCORE + ? where ID = ?',[result[0].SCORE,user]);
-                db.query('insert into Solved (PID,USER) values (?,?)',[pid,user]);
-                res.json({solve : "정답!!!٩(๑❛ワ❛๑)و"});
-            }
-            else
-                res.json({solve : "복습은 아주 좋은거죠 하지만 점수는 없어요ㅎ⁽⁽◝( ˙ ꒳ ˙ )◜⁾⁾"});
+            db.query('select * from Solved where PID = ? and USER = ?',[pnum,user],(error,results) => {
+                if(error) console.log(error);
+                if(results.length === 0){
+                    db.query('update Users set SCORE=? where ID = ?',[result[0].SCORE + req.session.score,user]);
+                    db.query('insert into Solved (PID,USER) values (?,?)',[pnum,user]);
+                    res.json({solve : "정답!!!٩(๑❛ワ❛๑)و"});
+                }
+                else{
+                    res.json({solve : "복습은 아주 좋은거죠 하지만 점수는 없어요ㅎ⁽⁽◝( ˙ ꒳ ˙ )◜⁾⁾"});
+                }
+            })
         }
-        else
+        else{
             res.json({solve : "정답이 아니에요....૮(꒦ິ ˙̫̮ ꒦ິ)ა"});
+        }
         
     })
 })
