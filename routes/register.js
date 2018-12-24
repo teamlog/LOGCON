3abbeb918b2d0e3a1a01dd26e0786025c67aaae8
giljong <minjong5054@daum.net>
2018-12-24 17:32:22 +0900

    db 구조, 회원가입 고등학생, 중학생 구분

--- routes/register.js
+++ routes/register.js
@@ -24,6 +24,7 @@ router.get('/',(req,res) => {
     const pw = req.body.pw;
     const tmpEmail = req.body.email;
     const tmpSchool = req.body.school;
+    var tmpGrade = req.body.grade;
     const tmpPwd = crypto.createHash('sha512').update(pw).digest('base64');
     function emailCheck(email){
         var regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
@@ -32,7 +33,16 @@ router.get('/',(req,res) => {
         else 
             return 0;
     }
-    if(tmpId===''||tmpPwd===''||tmpEmail===''||tmpSchool === '')
+    function gradeCheck(grade){
+        if(grade === 'm')
+            grade = "중학생";
+        if(grade === 'h')
+            grade = "고등학생";
+        else
+            res.json({success : false});
+    }
+    gradeCheck(tmpGrade);
+    if(tmpId===''||tmpPwd===''||tmpEmail===''||tmpSchool === ''||tmpGrade === '')
         res.json({success: false});
     if(pw.length<8||pw.length>20||tmpId.length>20||tmpId.length<5)
         res.json({success:false})
@@ -50,7 +60,7 @@ router.get('/',(req,res) => {
                         res.json({success: false});
                     else{
                         const authkey = randomstring.generate();
-                        db.query('insert into Users (ID,PW,EMAIL,SCHOOL,AUTHKEY) values (?,?,?,?,?)',[tmpId,tmpPwd,tmpEmail,tmpSchool,authkey]);
+                        db.query('insert into Users (ID,PW,EMAIL,SCHOOL,AUTHKEY) values (?,?,?,?,?,?)',[tmpId,tmpPwd,tmpEmail,tmpSchool,authkey,tmpGrade]);
                         const transporter = nodemailer.createTransport({
                             service: 'Gmail',
                             auth: {
