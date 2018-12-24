ea38087cf01c42fb29e45de266a3f34c2292c4e4
giljong <minjong5054@daum.net>
2018-12-25 00:09:24 +0900

    주석해제

--- app.js
+++ app.js
@@ -50,20 +50,20 @@ app.set('view engine', 'ejs');
 app.use(helmet()); 
 
 // catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-//  next(createError(404));
-//});
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-//app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
- // res.locals.message = err.message;
-  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
- // res.status(err.status || 500);
- // res.render('404.ejs');
-//});
+  res.status(err.status || 500);
+  res.render('404.ejs');
+});
 
 app.listen(4000, () => {
   console.log("connect");
