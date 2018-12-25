cc65214909cb6c2a00644cafb599c6764b6354b6
giljong <minjong5054@daum.net>
2018-12-25 22:36:46 +0900

    이멜 재인증

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const db = require('../db/connection');
 const router = express.Router();
+const randomstring = require('randomstring');
 
 router.get('/', (req,res) => {
     if(!(req.session.user === undefined)){
@@ -16,72 +17,64 @@ router.get('/', (req,res) => {
         }
     }
     else
-        redirect('/');
+        res.redirect('/');
 })
 .post('/', (req,res) => {
-    const key = req.body.key; 
-    db.query('select AUTHKEY from Users where ID = ?',req.session.user,(err,result) => {
-        if(err) throw err;
-        if((key === result[0].AUTHKEY)){
-            db.query('update Users set FLAG=1 where ID = ?',req.session.user);
-            req.session.flag = 1;
-            req.session.save(() => {
-                res.json({success : true});
+    if(req.session.flag){
+        res.json({success : 'already'})
+    }
+    else{
+        const key = req.body.key; 
+        const email = req.body.email;
+        console.log(key,email)
+        if(!(req.body.key===undefined)){
+            db.query('select AUTHKEY from Users where ID = ?',req.session.user,(err,result) => {
+                if(err) throw err;
+                if((key === result[0].AUTHKEY)){
+                    db.query('update Users set FLAG=1 where ID = ?',req.session.user);
+                    req.session.flag = 1;
+                    req.session.save(() => {
+                        res.json({success : true});
+                    })
+                }
+                else
+                    res.json({success:false});
             })
         }
-        else
-            res.json({success:false});
-    })
-})
-
-router.get('/reSend',(req,res) => {
-    if(!(req.session.user === undefined)){
-        if(!(req.session.flag)){
-            res.render('reSend.ejs',{
-                id:req.session.user,
-                school : req.session.school
-            });
-        }
         else{
-            res.redirect('/');
+            db.query('select * from Users where EMAIL = ?',req.body.email,(err,result) => {
+                if(err) console.log(err);
+                if(!(result.length === 0))
+                    res.json({success : false});
+                else{
+                    const authkey = randomstring.generate();
+                    db.query('update Users set AUTHKEY = ?, EMAIL = ? where ID = ?',[authkey,email,req.session.user]);
+                    res.json({success: true});
+                    const transporter = nodemailer.createTransport({
+                        service: 'Gmail',
+                        auth: {
+                            user: 'teamlogsr@gmail.com', 
+                            pass: 'teamlogzzang2017'
+                        }
+                    });
+                    const mailOptions = {
+                        from: 'teamlogsr@gmail.com',
+                        to: email ,
+                        subject: 'LOGCON 인증',
+                        text: '가입완료를 위해 <'+authkey+'> 를 입력해주세요'
+                    };
+                    transporter.sendMail(mailOptions, (err, response) => {
+                        if(err)
+                            console.log(err);
+                        else{
+                            console.log('sibal',response);
+                        }   
+                    })
+                }
+            })
         }
     }
-    else
-        redirect('/');
 })
 
-router.post('/reSend',(req,res) => {
-    const email = req.body.email;
-    db.query('select SCORE from User where EMAIL = ?',email,(err,result) => {
-        if(err) console.log(err);
-        if(!(result.length === 0))
-            res.json({success : false});
-        else{
-            const authkey = randomstring.generate();
-            db.query('update Users set AUTHKEY = ?, EMAIL = ? where ID = ?',[authkey,email,req.session.id]);
-            res.json({success: true});
-            const transporter = nodemailer.createTransport({
-                service: 'Gmail',
-                auth: {
-                    user: 'teamlogsr@gmail.com', 
-                    pass: 'teamlogzzang2017'
-                }
-            });
-            const mailOptions = {
-                from: 'teamlogsr@gmail.com',
-                to: tmpEmail ,
-                subject: 'LOGCON 인증',
-                text: '가입완료를 위해 <'+authkey+'> 를 입력해주세요'
-            };
-            transporter.sendMail(mailOptions, (err, response) => {
-                if(err)
-                    console.log(err);
-                else{
-                    console.log('sibal',response);
-                }   
-            })
-        }
-    })
-})
 
 module.exports = router;
