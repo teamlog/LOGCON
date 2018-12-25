21f104ec849087afad595c79e68b396b2d3602c9
giljong <minjong5054@daum.net>
2018-12-25 18:54:00 +0900

    이메일 재인증 만듦

--- routes/auth.js
+++ routes/auth.js
@@ -14,6 +14,8 @@ router.get('/', (req,res) => {
             res.redirect('/');
         }
     }
+    else
+        redirect('/');
 })
 .post('/', (req,res) => {
     const key = req.body.key; 
@@ -29,6 +31,56 @@ router.get('/', (req,res) => {
         else
             res.json({success:false});
     })
-}) 
+})
+
+router.get('/reSend',(req,res) => {
+    if(!(req.session.user === undefined)){
+        if(!(req.session.flag)){
+            res.render('reSend.ejs',{
+                id:req.session.user,
+                school : req.session.school
+            });
+        }
+        else{
+            res.redirect('/');
+        }
+    }
+    else
+        redirect('/');
+})
+
+router.post('/reSend',(req,res) => {
+    const email = req.body.email;
+    db.query('select SCORE from User where EMAIL = ?',email,(err,result) => {
+        if(err) console.log(err);
+        if(!(result.length === 0))
+            res.json({success : false});
+        else{
+            const authkey = randomstring.generate();
+            db.query('update Users set AUTHKEY = ?, EMAIL = ? where ID = ?',[authkey,email,req.session.id]);
+            res.json({success: true});
+            const transporter = nodemailer.createTransport({
+                service: 'Gmail',
+                auth: {
+                    user: 'teamlogsr@gmail.com', 
+                    pass: 'teamlogzzang2017'
+                }
+            });
+            const mailOptions = {
+                from: 'teamlogsr@gmail.com',
+                to: tmpEmail ,
+                subject: 'LOGCON 인증',
+                text: '가입완료를 위해 <'+authkey+'> 를 입력해주세요'
+            };
+            transporter.sendMail(mailOptions, (err, response) => {
+                if(err)
+                    console.log(err);
+                else{
+                    console.log('sibal',response);
+                }   
+            })
+        }
+    })
+})
 
 module.exports = router;
